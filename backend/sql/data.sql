878d2d964c8f82aa3909df8442dae39d1dd94d92
Pete <pheidler@ucsc.edu>
2020-12-10 01:09:25 -0800

    custom avatars work now -- need to add profile page next

--- backend/sql/data.sql
+++ backend/sql/data.sql
@@ -2,10 +2,10 @@
 -- Populate Your Tables Here --
 
 --USERS--
-INSERT INTO users(email, passwordHash, mailboxes, profilePicture, name, showAvatar) VALUES ('anna@books.com', '$2b$10$Y00XOZD/f5gBSpDusPUgU.G1ohpR3oQbbBHK4KzX7dU219Pv/lzze', '{ Inbox, Trash, Sent, Starred, Drafts, New Mailbox }', 'https://res.cloudinary.com/rrees/image/upload/v1606098596/fim0bxki72rhoxvkvyhb.jpg', 'Pete Heidler', true);
+INSERT INTO users(email, passwordHash, mailboxes, profilePicture, name, showAvatar) VALUES ('pheidler@ucsc.edu', '$2b$10$Y00XOZD/f5gBSpDusPUgU.iJufk6Nxx6gAoHRG8t2eHyGgoP2bK4y', '{ Inbox, Trash, Sent, Starred, Drafts, Custom Mailbox }', 'https://res.cloudinary.com/rrees/image/upload/v1607544879/ql0ecyz5ocouitkbcs8z.jpg', 'Pete Heidler', true);
+
+INSERT INTO users(email, passwordHash, mailboxes, profilePicture, name, showAvatar) VALUES ('dcharris@ucsc.edu', '$2b$10$Y00XOZD/f5gBSpDusPUgU.G1ohpR3oQbbBHK4KzX7dU219Pv/lzze', '{ Inbox, Trash, Sent, Starred, Drafts }', '', 'David Harrison', true);
 
 
 --EMAILS--
-INSERT INTO mail(mailbox, email, mail) VALUES ('Inbox', 'anna@books.com', '{"to":{"name":"Shandra Rheam","email":"anna@books.com"},"from":{"name":"CSE183 Student","email":"cse183student@ucsc.edu"},"received":"2020-11-17T23:17:19Z","sent":"2020-11-14T17:09:17Z","content":"Cras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque.\n\nQuisque porta volutpat erat. Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus.","subject":"Sent sent sent"}');
-
-INSERT INTO mail(mailbox, email, mail) VALUES ('New Mailbox', 'anna@books.com', '{"to":{"name":"Shandra Rheam","email":"srheam0@myspace.com"},"from":{"name":"CSE183 Student","email":"cse183student@ucsc.edu"},"received":"2019-11-17T23:17:19Z","sent":"2019-12-14T17:09:17Z","content":"Cras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque.\n\nQuisque porta volutpat erat. Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus.","subject":"Sent sent sent"}');
+INSERT INTO mail(mailbox, email, mail) VALUES ('Inbox', 'pheidler@ucsc.edu', '{"to":{"name":"Pete Heidler","email":"pheidler@ucsc.edu"},"from":{"name":"David Harrison","email":"dcharris@ucsc.edu"},"received":"2020-11-17T23:17:19Z","sent":"2020-11-14T17:09:17Z","content":"Wow Pete, I am beyond impressed with you. Keep up the great work!","subject":"A message from your Professor"}');
